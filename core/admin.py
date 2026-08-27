b3f69b3cf15db5d4e8ce4a0054ba68db11985c55
ram <rameshwarb.006@gmail.com>
2026-08-27 09:00:30 +0530

    feat: register core models in Django admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Executive, Party, Product, Subproduct, Invoice, MOU
 
-# Register your models here.
+
+@admin.register(Executive)
+class ExecutiveAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Party)
+class PartyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'state', 'executive')
+    list_filter = ('state', 'executive')
+    search_fields = ('name', 'city')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Subproduct)
+class SubproductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'product')
+    list_filter = ('product',)
+    search_fields = ('name',)
+
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('invoice_no', 'date', 'party', 'product', 'subproduct', 'basic_amount', 'fiscal_year')
+    list_filter = ('fiscal_year', 'month', 'product')
+    search_fields = ('invoice_no', 'party__name')
+    date_hierarchy = 'date'
+
+
+@admin.register(MOU)
+class MOUAdmin(admin.ModelAdmin):
+    list_display = ('party', 'fiscal_year', 'annual_target')
+    list_filter = ('fiscal_year',)
+    search_fields = ('party__name',)
