06eaf7ded1af8a341300b349904e36745c8bcbdb
ram <rameshwarb.006@gmail.com>
2026-09-05 10:36:38 +0530

    feat: accounts - login/logout views and templates

--- config/settings.py
+++ config/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/data/upload/'   # temporary - we'll point this to the dashboard once it exists
+LOGIN_URL = '/login/'
